16b3fc41e051991fb4d6148ef0c1cd863c8faa6c
fmoscatiello <farid.moscatiello@outlook.fr>
2024-02-19 11:46:18 +0100

    modifcation src

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,14 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { CardListComponent } from "./card-list/card-list.component";
-import { FormationPersonnaliseeComponent } from "../../commons/formation-personnalisee/formation-personnalisee.component";
+import { FinancementListComponent } from "./financement-list/financement-list.component";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.css',
-    imports: [HttpClientModule, BannerComponent, CardListComponent, FormationPersonnaliseeComponent]
+    imports: [HttpClientModule, BannerComponent, CardListComponent, FinancementListComponent]
 })
 export class HomePageComponent {
 
